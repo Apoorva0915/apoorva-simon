ba4846919ce0ce6562ad1a9563f8faa901d1405a
Apoorva Kesarwani <129680963+Apoorva0915@users.noreply.github.com>
2023-05-11 01:17:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let userClickedPattern = [];
 let started = false;
 let level = 0;
 
-$(document).keypress(function () {
+$(".start").click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -15,13 +15,14 @@ $(document).keypress(function () {
   }
 });
 
-$(".btn").click(function () {
+$(".btn").click(function() {
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
-  checkAnswer(userClickedPattern.length - 1);
+  checkAnswer(userClickedPattern.length-1);
 });
+
 
 
 function nextSequence() {
@@ -48,33 +49,34 @@ function animatePress(currentColor) {
 }
 
 function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("success");
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      console.log("success");
 
-    if (userClickedPattern.length === gamePattern.length) {
-      setTimeout(function () {
-        nextSequence();
-      }, 1000);
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
 
+      }
+
+    } else {
+      console.log("wrong");
+      
+      playSound("wrong")
+
+      $("body").addClass("game-over")
+      setTimeout(function(){
+        $("body").removeClass("game-over")
+      },200)
+
+      $("#level-title").text("Game Over, Click On Play Game to Restart")
+      startOver()
+      $("#level-title").css("line-height","63px")
     }
-
-  } else {
-    console.log("wrong");
-
-    playSound("wrong")
-
-    $("body").addClass("game-over")
-    setTimeout(function () {
-      $("body").removeClass("game-over")
-    }, 200)
-
-    $("#level-title").text("Game Over, Press Any Key to Restart")
-    startOver()
-  }
 }
 
-function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
 }
